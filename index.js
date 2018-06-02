09d7ef9c1b12a18c7fb637bdf1345f8187717401
Stupid7 <sudieep661@gmail.com>
2018-06-02 15:13:16 +0545

    Ready for singlePageApp

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var app = express();
+var todoRoutes = require("./routes/todos");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res){
   res.json({message:"Hello From JS Object"});
 });
 
-
+app.use("/api/todos", todoRoutes);
 
 
 app.listen(3000,function(){
---
+++ models/todo.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+var todoSchema = new mongoose.Schema({
+  name:{
+        type:String,
+        required:"Name Cannot be blank!!"
+  },
+  completed:{
+    type:Boolean,
+    default:false
+  },
+  created_date:{
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+var Todo = mongoose.model("Todo", todoSchema);
+
+module.export = Todo;
